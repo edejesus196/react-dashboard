d59b74546cf9486bbfe82a289cfc98f1acff590f
Edward De Jesus <edejesus196@gmail.com>
2022-11-17 17:47:00 -0500

    First Commit

---
+++ src/contexts/ContextProvider.js
@@ -0,0 +1,20 @@
+import React, {createContent, useContext, useState } from 'react';
+
+const StateContext =createContext();
+
+const initialState = {
+    chat: false,
+    cart: false,
+    userProfile: false,
+    notification: false,
+}
+
+export const ContextProvider = ({ children }) => {
+    return (
+        <StateContext.Provider
+            value={{ test: 'test'}}
+        >
+            {children}
+        </StateContext.Provider>
+    )
+}
